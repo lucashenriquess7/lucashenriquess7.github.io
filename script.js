b31224858d0e24aedeaee89b337aaf122c80529e
lucashenriquess7 <lucashenriquess7@hotmail.com>
2022-05-22 23:42:22 -0300

    ajustes

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
 
-// document.querySelector("#qtde").addEventListener("change", atualizarPreco)
-// document.querySelector("#js").addEventListener("change", atualizarPreco)
-// document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
-// document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
-// document.querySelector("#prazo").addEventListener("change", function () {
-//     const prazo = document.querySelector("#prazo").value
-//     document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
-//     atualizarPreco()
-// })
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+})
 
 function atualizarPreco(){
     const qtde = document.querySelector("#qtde").value
